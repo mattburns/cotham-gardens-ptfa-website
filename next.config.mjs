9fd9d0f49924cf56bad359efe27bdda75878789c
Matt Burns <matt@cameraforensics.com>
2025-03-22 14:38:51 +0000

    Fix basePath and distDir formatting in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 export default {
   output: 'export',
-  basePath: '/cotham-gardens-ptfa-website/',
-  distDir: 'out/cotham-gardens-ptfa-website/',
+  basePath: '/cotham-gardens-ptfa-website',
+  distDir: 'out/cotham-gardens-ptfa-website',
   trailingSlash: true,
   images: { unoptimized: true },
   webpack: (cfg) => {
